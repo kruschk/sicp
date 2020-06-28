2c090fb77890e6d49895b81ab4768a4c9a6c9e89
Jonah Kruschke <jonah.kruschke@gmail.com>
2020-06-27 20:27:23 -0400

    Read up to the end of chapter 4.2
    - Read up to the end of chapter 4.2.
    - Complete selected exercises.

--- book/ch4/ch4.2.scm
+++ book/ch4/ch4.2.scm
@@ -2,3 +2,160 @@
 ; Variations on a Scheme -- Lazy Evaluation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (display "Variations on a Scheme -- Lazy Evaluation\n")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 4.2.1 Normal Order and Applicative Order
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(display "\n4.2.1 Normal Order and Applicative Order\n")
+
+(define (try a b)
+  (if (= a 0) 1 b))
+
+;(define (unless condition usual-value exceptional-value)
+;  (if condition exceptional-value usual-value))
+
+;(let ((a 7) (b 3))
+;  (unless (= b 0)
+;    (/ a b)
+;    (begin (display "exception: returning 0")
+;           0)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 4.2.2 An Interpreter with Lazy Evaluation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(display "\n4.2.2 An Interpreter with Lazy Evaluation\n")
+
+; Modifying the evaluator
+(display "\nModifying the evaluator\n")
+
+;((application? exp)
+; (apply (actual-value (operator exp) env)
+;        (operands exp)
+;        env))
+
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
+(define (apply procedure arguments env)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure
+           procedure
+           (list-of-arg-values arguments env))) ; changed
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             (list-of-delayed-args arguments env) ; changed
+             (procedure-environment procedure))))
+        (else (error "Unknown procedure type -- APPLY" procedure))))
+
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (actual-value (first-operand exps) env)
+            (list-of-arg-values (rest-operands exps)
+                                env))))
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it (first-operand exps) env)
+            (list-of-delayed-args (rest-operands exps)
+                                  env))))
+
+(define (eval-if exp env)
+  (if (true? (actual-value (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define input-prompt ";;; L-Eval input:")
+(define output-prompt ";;; L-Eval value:")
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output
+            (actual-value input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+;(define the-global-environment (setup-environment))
+;(driver-loop)
+;(define (try a b)
+;  (if (= a 0) 1 b))
+;(try 0 (/ 1 0))
+
+; Representing thunks
+(display "\nRepresenting thunks\n")
+
+(define (force-it obj)
+  (if (thunk? obj)
+      (actual-value (thunk-exp obj) (thunk-env obj))
+      obj))
+
+(define (delay-it exp env)
+  (list 'thunk exp env))
+(define (thunk? obj)
+  (tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk) (cadr evaluated-thunk))
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result (actual-value (thunk-exp obj)
+                                     (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           (set-car! (cdr obj) result) ; replace exp with its value
+           (set-cdr! (cdr obj) '())    ; forget unneeded env
+           result))
+        ((evaluated-thunk? obj)
+         (thunk-value obj))
+        (else obj)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 4.2.3 Streams as Lazy Lists
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(display "\n4.2.3 Streams as Lazy Lists\n")
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+(define (car z)
+  (z (lambda (p q) p)))
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
+(define (add-lists list1 list2)
+  (cond ((null? list1) (list2))
+        ((null? list2) (list1))
+        (else (cons (+ (car list1) (car list2))
+                    (add-lists (cdr list1) (cdr list2))))))
+;(define ones (cons 1 ones))
+;(define integers (cons 1 (add-lists ones integers)))
+
+(define (integral integrand initial-value dt)
+  (define int
+    (cons initial-value
+          (add-lists (scale-list integrand dt)
+                     int)))
+  int)
+;(define (solve f y0 dt)
+;  (define y (integral dy y0 dt))
+;  (define dy (map f y))
+;  y)
---
+++ book/ch4/e4.25.scm
@@ -0,0 +1,22 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Exercise 4.25
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(display "Exercise 4.25\n")
+
+(define (unless condition usual-value exceptional-value)
+  (if condition exceptional-value usual-value))
+
+(define (factorial n)
+  (unless (= n 1)
+    (* n (factorial (- n 1)))
+    1))
+
+(display (factorial 5)) (newline)
+
+; Since Scheme uses applicative order, evaluating `(factorial 5)` will never
+; halt because the interpreter will try to evaluate all of the arguments to
+; `unless` when the procedure is applied. This will result in an infinite
+; recursion because the interpreter will repeatedly try to evaluate
+; `(* n (factorial (- n 1)))` even when it should stop. In normal-order
+; evaluation, however, the recursive step will only be performed when
+; needed, thus the procedure may halt.
